8c96e20e28bb8c9fe23ab9e95c6f35ff4400b439
Beelzebob1 <94956634+Beelzebob1@users.noreply.github.com>
2023-06-07 22:30:57 +0200

    first commit

---
+++ nav.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="CSS/nav.css">
+<nav>
+    <ul>
+        <li><a href="Home.php">Home</a></li>
+        <li><a href="boeken.php">Boeken</a></li>
+        <li><a href="reserveerde.php">Gereserveerde boeken</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+    <button>Log In</button>
+</nav>
